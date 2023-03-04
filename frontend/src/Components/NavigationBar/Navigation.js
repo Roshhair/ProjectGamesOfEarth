aa046d8379d3dfbfc67bae73cabbafbaf298cf00
Roshhair <rvm2000m@gmail.com>
2023-03-05 03:16:56 +0530

    Navbar Options with Map Function

--- frontend/src/Components/NavigationBar/Navigation.js
+++ frontend/src/Components/NavigationBar/Navigation.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import './Navigation.css'
+// import  img from './hamburgur.svg'
 
 export const Navigation = (props) => {
+    const data=["About Us","Blog","Services","Home"];
     // const {home,services,aboutus,blog}=props
     return (
         <>
             <nav className='innerdivnav'>
                 <div>
-                    {/* <p className='text'>GAMESOFEARTH</p> */}
                     <ul className='nav_li'>
                         <li ><a href='/' className='nav_head text_only'>GAMESOFEARTH</a></li>
                         <li className='nav_text'><button className=''>Login</button></li>
-                        <li ><a href='/'className='nav_text text_only' >About Us</a></li>
-                        <li ><a href='/' className='nav_text text_only' >Blog</a></li>
-                        <li ><a href='/'  className='nav_text text_only'>Services</a></li>
-                        <li ><a href='/' className='nav_text text_only'>Home</a></li>
+                        {data.map((data)=>{
+                            return <li ><a href='/' className='nav_text text_only' >{data}</a></li>
+                        })}
                     </ul>
                 </div>
             </nav>
